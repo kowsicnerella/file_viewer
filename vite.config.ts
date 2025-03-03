fb0219a8518117dd6b8051269e3dfa69dd163d01
VK22 <kowsic.nerella@gmail.com>
2025-03-03 10:06:27 +0530

    update final

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  server: {
-    host: '10.56.8.125', // This allows access via any network IP, use your specific IP if needed
-    port: 5173,       // You can change this to any port you prefer
-    open: true,       // Automatically open the app in the browser
-  },
+//   server: {
+//     host: '10.56.8.125', // This allows access via any network IP, use your specific IP if needed
+//     port: 5173,       // You can change this to any port you prefer
+//     open: true,       // Automatically open the app in the browser
+//   },
 });
